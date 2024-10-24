c595ab3ee114759b3dacc423060a925786e85a5f
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-24 21:14:54 +0300

    Добавьте файлы проекта.

---
+++ Homework_NP_7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Homework_NP_7
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<string> recipients = new List<string>();
+
+
+            MailAddress from = new MailAddress("somemail@gmail.com", "Tom");
+            MailMessage message = new MailMessage()
+            {
+                Subject = "Test Message",
+                Body = "<h2>Mail test of smtp client operation</h2>",
+                IsBodyHtml = true,
+                From = from
+            };
+
+            foreach (string recipient in recipients)
+            {
+                message.To.Add(recipient);
+            }
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("somemail@gmail.com", "Application Password");
+            smtp.EnableSsl = false;
+            smtp.Send(message);
+        }
+    }
+}
